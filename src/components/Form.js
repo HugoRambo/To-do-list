bc9d4057f1983a5dcebd2192f1634e690eb71038
HugoRambo <hugo.rambo@hotmail>
2022-05-27 18:00:16 -0300

    second part

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,38 @@
-import React from 'react'
+import React, {useEffect} from "react"
 import {v4 as uuidv4} from "uuid"
-const Form =({input, setInput, todos, setTodos}) =>{
+const Form =({input, setInput, todos, setTodos, editTodo, setEditTodo}) =>{
+    const updateTodo = (title, id, completed) => {
+        const newTodo = todos.map((todo) =>
+            todo.id === id ?{ title, id, completed } : todo
+        )
+        setTodos(newTodo)
+        setEditTodo("")
+        }
+    useEffect (() =>{
+        if(editTodo){
+            setInput(editTodo.title)
+        }else{
+            setInput("")
+        }
+    }, [setInput, editTodo])
+    
     const oninputChange = (event) =>{
         setInput(event.target.value)
     } 
     const onFormSubmit = (event) =>{
         event.preventDefault()
-        setTodos([...todos, {id: uuidv4(), title: input, completed:false}])
+        if(!editTodo){
+            setTodos([...todos, {id: uuidv4(), title: input, completed:false}])
         setInput("")
+        }else{
+            updateTodo(input, editTodo.id, editTodo)
+        }
+        
     }
     return(
         <form onSubmit={onFormSubmit}>
             <input type="text" placeholder='Digite...' className="task-input" value={input} required onChange={oninputChange}/>
-            <button className="button-add" type='submit'>ADD</button>
+            <button className="button-add" type='submit'>{editTodo ? "OK" :"add"}</button>
         </form>
     )
 }
